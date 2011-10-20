56eb87999f5c348aa2994bd54e0e3e4f0ba4877c
Suraj N. Kurapati <sunaku@gmail.com>
2011-10-20 00:24:15 -0700

    zsh: make ^W stop at non-alphanumeric characters

--- .zsh/config/1-interaction.zsh
+++ .zsh/config/1-interaction.zsh
@@ -4,5 +4,9 @@ eval "$(sed -n 's/^/bindkey /; s/: / /p' /etc/inputrc)"
 # do not erase entire line when Control-U is pressed
 bindkey '^U' backward-kill-line
 
+# make ^W stop at non-alphanumeric characters
+autoload -U select-word-style
+select-word-style bash
+
 # keep command history unique to fit more items in it!
 setopt histignorealldups
