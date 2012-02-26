c39b23614560dddfd8362740774e4ec28f691475
Suraj N. Kurapati <sunaku@gmail.com>
2012-02-25 16:02:30 -0800

    zshrc: disable fancy precmd() in Linux VT console

--- .zsh/config/precmd.zsh
+++ .zsh/config/precmd.zsh
@@ -1,3 +1,5 @@
-# show current command, directory, and user in terminal title
-precmd()  { print -Pn "\e]2;$0 (%~) %n@%m\a" }
-preexec() { print -Pn "\e]2;$1 (%~) %n@%m\a" }
+if test "$TERM" != linux; then
+  # show current command, directory, and user in terminal title
+  precmd()  { print -Pn "\e]2;$0 (%~) %n@%m\a" }
+  preexec() { print -Pn "\e]2;$1 (%~) %n@%m\a" }
+fi
