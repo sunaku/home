4351be2138a32d007b0e8e86c701352c4c7af9a1
Suraj N. Kurapati <sunaku@gmail.com>
2012-02-29 18:00:46 -0800

    alias: only apply ZSH aliases if we're running ZSH

--- .alias.d/zsh.sh
+++ .alias.d/zsh.sh
@@ -1,4 +1,6 @@
-alias -g G='| grep'
-alias -g V="| $PAGER"
-alias -g T='| tee /dev/tty |'
-alias -g N='/dev/null'
+if test "${SHELL##*/}" = zsh; then
+  alias -g G='| grep'
+  alias -g V="| $PAGER"
+  alias -g T='| tee /dev/tty |'
+  alias -g N='/dev/null'
+fi
