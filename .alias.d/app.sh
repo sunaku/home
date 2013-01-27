90226d6db49a5df3621dda260bdd5c644be924fd
Suraj N. Kurapati <sunaku@gmail.com>
2013-01-26 18:08:50 -0800

    make rsync(1) honor ~/.cvsignore by passing it -C

--- .alias.d/app.sh
+++ .alias.d/app.sh
@@ -10,6 +10,6 @@ alias b=$BROWSER
 alias x='nice extract-archive'
 
 alias open='xdg-open'
-alias scp='rsync --rsh=ssh -arvP'
+alias scp='rsync --rsh=ssh -CarvP'
 alias sloc='cloc --by-file-by-lang --exclude-dir .git'
 alias diff='colordiff -u'
