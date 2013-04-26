ef995ac9b3c255cdb67ee53ac069ccc97d5dddac
Suraj N. Kurapati <sunaku@gmail.com>
2013-04-26 10:50:49 -0700

    zsh-history-substring-search no longer binds keys by default

--- .zsh/bundle/zsh-history-substring-search.zsh
+++ .zsh/bundle/zsh-history-substring-search.zsh
@@ -2,3 +2,18 @@ if test $TERM != linux; then
   HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='fg=yellow,bold,standout'
   HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND='fg=red,bold,standout'
 fi
+
+# bind UP and DOWN arrow keys
+for keycode in '[' '0'; do
+  bindkey "^[${keycode}A" history-substring-search-up
+  bindkey "^[${keycode}B" history-substring-search-down
+done
+unset keycode
+
+# bind P and N for EMACS mode
+bindkey -M emacs '^P' history-substring-search-up
+bindkey -M emacs '^N' history-substring-search-down
+
+# bind k and j for VI mode
+bindkey -M vicmd 'k' history-substring-search-up
+bindkey -M vicmd 'j' history-substring-search-down
