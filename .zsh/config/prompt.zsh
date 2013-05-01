81a8d5eef144c38479111b1c1397c254626da9ca
Suraj N. Kurapati <sunaku@gmail.com>
2013-05-01 11:13:57 -0700

    zsh prompt: use '%' instead of '!' to show changes

--- .zsh/config/prompt.zsh
+++ .zsh/config/prompt.zsh
@@ -14,7 +14,7 @@ RPROMPT='%F{cyan}%@%f'
 autoload -Uz vcs_info
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:*' stagedstr     '%B%F{green}$%f%b'
-zstyle ':vcs_info:*' unstagedstr   '%B%F{yellow}!%f%b'
+zstyle ':vcs_info:*' unstagedstr   '%B%F{yellow}%%%f%b'
 zstyle ':vcs_info:*' formats       '%c%u%b%m '
 zstyle ':vcs_info:*' actionformats '%c%u%b%m %B%s-%a%%b '
 zstyle ':vcs_info:git*+set-message:*' hooks git-untracked git-aheadbehind git-remotebranch
