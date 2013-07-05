a3ca1039a25340a5f7ca22af9d2e8a2a83999f2b
Suraj N. Kurapati <sunaku@gmail.com>
2013-07-05 09:09:58 -0700

    ZSH history substring search: use terminfo keybinds

--- .zsh/bundle/zsh-history-substring-search.zsh
+++ .zsh/bundle/zsh-history-substring-search.zsh
@@ -1,14 +1,18 @@
-if test $TERM != linux; then
+if test "$TERM" != linux; then
   HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='fg=yellow,bold,standout'
   HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND='fg=red,bold,standout'
 fi
 
 # bind UP and DOWN arrow keys
-for keycode in '[' '0'; do
-  bindkey "^[${keycode}A" history-substring-search-up
-  bindkey "^[${keycode}B" history-substring-search-down
-done
-unset keycode
+zmodload zsh/terminfo
+bindkey "$terminfo[kcuu1]" history-substring-search-up
+bindkey "$terminfo[kcud1]" history-substring-search-down
+
+# ... support zsh in tmux in URxvt too
+if test "${TERM#screen}" != "$TERM"; then
+  bindkey '^[[A' history-substring-search-up
+  bindkey '^[[B' history-substring-search-down
+fi
 
 # bind P and N for EMACS mode
 bindkey -M emacs '^P' history-substring-search-up
