045408d41e99c282d710d95b92cbaa9f6e2cdf97
Suraj N. Kurapati <sunaku@gmail.com>
2013-10-03 13:49:21 -0700

    precmd: silence stderr when setting terminal title

--- .zsh/config/precmd.zsh
+++ .zsh/config/precmd.zsh
@@ -1,5 +1,5 @@
 if test "$TERM" != linux; then
   # show current command, directory, and user in terminal title
-  precmd()  { print -Pn "\e]2;$0 (%~) %n@%m\a" }
-  preexec() { print -Pn "\e]2;$1 (%~) %n@%m\a" }
+  precmd()  { print -Pn "\e]2;$0 (%~) %n@%m\a" 2>/dev/null }
+  preexec() { print -Pn "\e]2;$1 (%~) %n@%m\a" 2>/dev/null }
 fi
