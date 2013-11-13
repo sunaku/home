ad64aaa185ca4105c5f509eec64958e89d7946e0
Suraj N. Kurapati <sunaku@gmail.com>
2013-11-13 15:29:43 -0800

    alias: add "grih" for interactive upstream rebase

--- .alias.d/git.sh
+++ .alias.d/git.sh
@@ -220,6 +220,7 @@ alias grc='git rebase --continue'
 alias gra='git rebase --abort'
 alias grs='git rebase --skip'
 alias grh='git rebase @{u}' # against upstream branch
+alias grih='grh --interactive'
 
 #-----------------------------------------------------------------------------
 # k = conflict
