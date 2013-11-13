fcfacdafb601088536f51477d70508a46c63d954
Suraj N. Kurapati <sunaku@gmail.com>
2013-11-13 12:12:57 -0800

    alias: rename "col" to "kut" for col(1) shadowing

--- .alias.d/text.sh
+++ .alias.d/text.sh
@@ -4,5 +4,6 @@ alias grep='grep --color'
 
 words() { grep "$@" /usr/share/dict/words ;}
 
+# this is similar to cut(1) but using awk(1) fields:
 # print only the given columns, numbered from 1 to N
-col() { awk "{ print $(for n; do echo -n "\$$n,"; done | sed 's/,$//') }" ;}
+kut() { awk "{ print $(for n; do echo -n "\$$n,"; done | sed 's/,$//') }" ;}
