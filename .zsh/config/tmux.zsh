36d2c8b8df47d370ecb487658e54aab57c5ba7da
Suraj N. Kurapati <sunaku@gmail.com>
2014-04-16 15:35:05 -0700

    tmux: replace X11 clipboarding with OSC 52 escape

--- .zsh/config/tmux.zsh
+++
@@ -1,6 +0,0 @@
-# Lets you inject tmux environment into existing shells via SIGUSR1.
-# For example, to update all existing ZSH sessions: `pkill -USR1 zsh`
-function eval-tmux-environment() {
-  eval $(tmux show-environment | sed 's/^\w/export "&/; s/^-/unset "/; s/$/"/')
-}
-trap eval-tmux-environment SIGUSR1
