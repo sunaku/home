2967b184c60337c68eb07c76242d776e69ce8df0
Suraj N. Kurapati <sunaku@gmail.com>
2014-07-06 23:17:51 -0700

    make simple backup aliases allow a trailing slash
    $ bam ref/
    mv: cannot move ‘ref/’ to a subdirectory of itself, ‘ref/.bak’
    exit 1
    
    $ bum ref.bak/
    mv: cannot move ‘ref.bak/’ to a subdirectory of itself, ‘ref.bak/ref.bak’
    exit 1

--- .alias.d/file.sh
+++ .alias.d/file.sh
@@ -18,24 +18,28 @@ alias tree='tree -ACF'
 
 bam() { # backup with move
   for file; do
+    file=${file%/}
     mv -v $file{,.bak}
   done
 }
 
 bum() { # undo backup move
   for file; do
+    file=${file%/}
     mv -v "$file" "${file%.bak}"
   done
 }
 
 bac() { # backup with copy
   for file; do
+    file=${file%/}
     cp -Rpv "$file" "$file~$(date -Ins)~"
   done
 }
 
 buc() { # undo backup copy
   for file; do
+    file=${file%/}
     dest=${file%%\~*}
     test -d "$dest" && mv -v "$dest" "$file.orig"
     mv -v "$file" "$dest"
