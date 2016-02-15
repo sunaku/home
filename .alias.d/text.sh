1cc85bcb928a0a68a47dc6927a15a71a1d015fc6
Suraj N. Kurapati <sunaku@riseup.net>
2016-02-15 08:37:42 -0800

    words: highlight matches & search all dictionaries

--- .alias.d/text.sh
+++ .alias.d/text.sh
@@ -2,7 +2,7 @@ alias puts='print -l'
 
 alias grep='grep --color'
 
-words() { egrep "$@" /usr/share/dict/words ;}
+words() { grep --color -E -i "$@" -R /usr/share/dict ;}
 
 # this is similar to cut(1) but using awk(1) fields:
 # print only the given columns, numbered from 1 to N
