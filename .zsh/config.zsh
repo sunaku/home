bb886245212203381e414256a55bebe2452a15d7
Suraj N. Kurapati <sunaku@riseup.net>
2017-04-25 12:37:11 -0700

    zsh: absorb ~/.alias files; update bundle loading

--- .zsh/config.zsh
+++ .zsh/config.zsh
@@ -1,12 +1,18 @@
 source ~/.pathrc
 
 # delegate configuration
-for file in ~/.zsh/config/**/*.zsh; do
-  source $file
+for config in ~/.zsh/config/**/*sh; do
+  source $config
 done
 
 # load plugin bundles
-for bundle in ~/.zsh/bundle/[0-9]/*; do
-  test -d $bundle && bundle=$bundle/${bundle##*/}.zsh
-  test -f $bundle && source $bundle
+source_if_exist() { test -s $1 && source $1 }
+for bundle in ~/.zsh/bundle/?/*; do
+  # load the bundle itself
+  source_if_exist $bundle/${bundle##*/}.plugin.zsh ||
+  source_if_exist $bundle/${bundle##*/}.zsh ||
+  source_if_exist $bundle/init.zsh
+
+  # load bundle configuration
+  source_if_exist $bundle.zsh || :
 done
--- .zsh/config/alias.zsh
+++ .zsh/config/alias.zsh
@@ -1,4 +1,2 @@
-test -f ~/.alias && source ~/.alias
-
 alias -g PAGE='| $PAGER'
 alias -g PEEK='| tee /dev/tty PAGE'
