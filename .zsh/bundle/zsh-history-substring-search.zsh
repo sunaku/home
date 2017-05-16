cc4b5accc40e2532ca9ac5bed2eef5c4b2b7ba3c
Suraj N. Kurapati <sunaku@riseup.net>
2017-05-16 13:28:41 -0700

    history-substring-search: fuzzy multi-word search

--- .zsh/bundle/zsh-history-substring-search.zsh
+++ .zsh/bundle/zsh-history-substring-search.zsh
@@ -1,3 +1,5 @@
+HISTORY_SUBSTRING_SEARCH_FUZZY=1
+
 if test "$TERM" != linux; then
   HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='fg=yellow,bold,standout'
   HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND='fg=red,bold,standout'
