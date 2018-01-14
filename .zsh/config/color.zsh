579bcab6381c2a5b28d5d0c0ba1311962d902378
Suraj N. Kurapati <sunaku@riseup.net>
2018-01-14 12:08:16 -0800

    less: don't override standout colors; use default

--- .zsh/config/color.zsh
+++ .zsh/config/color.zsh
@@ -5,7 +5,7 @@ unset LS_COLORS
 export LESS_TERMCAP_mb=$(tput bold; tput setaf 5) # start blink
 export LESS_TERMCAP_md=$(tput bold; tput setaf 3) # start bold
 export LESS_TERMCAP_me=$(tput sgr0) # turn off bold, blink and underline
-export LESS_TERMCAP_so=$(tput bold; tput setaf 0; tput setab 2) # start standout
+#export LESS_TERMCAP_so=$(tput bold; tput setaf 0; tput setab 2) # start standout
 export LESS_TERMCAP_se=$(tput rmso; tput sgr0) # stop standout
 export LESS_TERMCAP_us=$(tput smul; tput setaf 6) # start underline
 export LESS_TERMCAP_ue=$(tput rmul; tput sgr0) # stop underline
