ddae651ce6464e1116aee40a5340d60d65cbefb2
Suraj N. Kurapati <sunaku@riseup.net>
2019-05-21 11:05:20 -0700

    zsh: use add-zsh-hook to register precmd & preexec

--- .zsh/config/precmd.zsh
+++ .zsh/config/precmd.zsh
@@ -1,5 +1,9 @@
 if test "$TERM" != linux; then
   # show current command, directory, and user in terminal title
-  precmd()  { print -Pn "\e]2;$0 (%~) %n@%m\a" 2>/dev/null }
-  preexec() { print -Pn "\e]2;$1 (%~) %n@%m\a" 2>/dev/null }
+  _zsh_config_reset_title()  { print -Pn "\e]2;$0 (%~) %n@%m\a" }
+  _zsh_config_update_title() { print -Pn "\e]2;$1 (%~) %n@%m\a" }
+
+  autoload -Uz add-zsh-hook
+  add-zsh-hook precmd  _zsh_config_reset_title
+  add-zsh-hook preexec _zsh_config_update_title
 fi
