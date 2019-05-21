4bc12a3ae76d4ad124388570637b41827f75ca8c
Suraj N. Kurapati <sunaku@riseup.net>
2019-05-21 13:27:40 -0700

    zsh: enable `cdr` for navigating the chdir history

--- .zsh/config/chdir.zsh
+++ .zsh/config/chdir.zsh
@@ -8,3 +8,12 @@ setopt AUTO_PUSHD
 setopt PUSHD_IGNORE_DUPS
 setopt PUSHD_SILENT
 setopt PUSHD_TO_HOME
+
+#
+# Remembering Recent Directories
+#
+# http://zsh.sourceforge.net/Doc/Release/User-Contributions.html#Recent-Directories
+#
+autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
+add-zsh-hook chpwd chpwd_recent_dirs
+zstyle ':completion:*:*:cdr:*:*' menu selection
