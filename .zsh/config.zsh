be18b11c2e3737c92c44d4eebe1ec4adbe450b26
Suraj N. Kurapati <sunaku@riseup.net>
2019-12-16 12:12:06 -0800

    zsh: some bundles must be loaded AFTER user config
    https://github.com/zsh-users/zsh-syntax-highlighting/issues/67

--- .zsh/config.zsh
+++ .zsh/config.zsh
@@ -1,16 +1,23 @@
-# load plugin bundles
-source_if_exist() { test -s $1 && source $1 }
-for bundle in ~/.zsh/bundle/*; do
-  # load the bundle itself
-  source_if_exist $bundle/${bundle##*/}.plugin.zsh ||
-  source_if_exist $bundle/${bundle##*/}.zsh ||
-  source_if_exist $bundle/init.zsh
+source_if_exist() {
+  test -s $1 && source $1
+}
+source_bundles() {
+  for bundle; do
+    # load the bundle itself
+    source_if_exist $bundle/${bundle##*/}.plugin.zsh ||
+    source_if_exist $bundle/${bundle##*/}.zsh ||
+    source_if_exist $bundle/init.zsh
 
-  # load bundle configuration
-  source_if_exist $bundle.zsh ||:
-done
+    # load bundle configuration
+    source_if_exist $bundle.zsh ||:
+  done
+}
+source_configs() {
+  for config; do
+    source $config
+  done
+}
 
-# delegate configuration
-for config in ~/.zsh/config/**/*sh; do
-  source $config
-done
+source_bundles ~/.zsh/bundle/before/*
+source_configs ~/.zsh/config/**/*sh
+source_bundles ~/.zsh/bundle/after/*
