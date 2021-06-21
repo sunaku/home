6d72e7edac9747379887602bd9052b27f82eb342
Suraj N. Kurapati <sunaku@riseup.net>
2021-06-20 23:47:22 -0700

    zsh: switch from *.get to zsh-snap plugin manager

---
+++ .zsh/bundle/history-search-multi-word.zsh
@@ -0,0 +1,4 @@
+zstyle ':history-search-multi-word' highlight-color 'standout'
+# bindkey "^R" history-search-multi-word
+# bindkey "^S" history-search-multi-word-backwards
+bindkey "^S" history-search-multi-word
