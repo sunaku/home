d4ca82584fc0249c94df6588d22b3d04126b1d77
Suraj N. Kurapati <sunaku@riseup.net>
2021-08-16 14:41:36 -0700

    zsh: add https://github.com/Tarrasch/zsh-bd plugin

--- .zsh/config.zsh
+++ .zsh/config.zsh
@@ -21,6 +21,7 @@ znap source zsh-users/zsh-syntax-highlighting
 znap source zdharma/history-search-multi-word
 znap source zsh-users/zsh-autosuggestions
 znap source zsh-users/zsh-history-substring-search
+znap source Tarrasch/zsh-bd
 znap source agkozak/zsh-z
 
 znap source mafredri/zsh-async # for prompt.zsh
